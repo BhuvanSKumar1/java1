db66ca84064b2f0991973807ac1e0cfff4d7d3ba
BhuvanSKumar1 <bhuvan.23cs024@sode-edu.in>
2024-11-06 20:04:32 +0530

    Add files via upload

---
+++ M7.java
@@ -0,0 +1,10 @@
+package pkg1;
+import pkg1.Tools3;
+public class M7 {
+    public static void main(String[] args) {
+		Tools3 t1=new Tools3();
+		System.out.println(t1.reverse("Rohan "));
+
+	}
+
+}
